db0d92a1b90b5ef543418ac56bb0f2e7c4afae5d
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-05 13:42:19 +0100

    initial

---
+++ AsyncAwait/AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Aitor Iglesias Pubill on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
